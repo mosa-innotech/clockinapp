157348bf57b66c1b4604ece6f84c3ebccd28043b
JordanWebDev <JordanWebDev@users.noreply.github.com>
2019-04-26 11:28:57 -0600

    css for register page
    moved css file to public/css and linked css file on the app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('resources/views/auth/register.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/register.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
