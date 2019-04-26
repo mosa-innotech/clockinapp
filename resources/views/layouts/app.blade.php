deea9d3cc2df1258fa6a5471cc95252b5872393c
Meghabhatt23 <44779627+Meghabhatt23@users.noreply.github.com>
2019-04-26 11:41:01 -0600

    change bg color of login page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,10 +15,11 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="{{ URL::asset('css/login.css')}}" rel="stylesheet">
+
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ URL::asset('css/login.css')}}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
